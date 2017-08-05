008b35bc0df8a6768c94e81c7b7fe852dbbb343c
mmorandini <emmemorandini@gmail.com>
2017-08-05 13:35:53 +0100

    + implemented SoundCloud search + start working with Web Audio API

--- src/app/interfaces/Song.ts
+++ src/app/interfaces/Song.ts
@@ -1,12 +1,14 @@
 export interface Song
 {
 	id: number,
-	name: string,
+	title: string,
 	artist: string,
 	streamUrl: string,
 	provider: number,
 	idFromProvider: string,
 	duration: number,
 	imageUrl: string,
-	link: string
+	link: string,
+  waveform_url: string,
+  format: string
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,24 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import {Song} from '../interfaces/Song';
 import { HttpClient } from '@angular/common/http';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 // import 'rxjs/add/operator/map';
-
+import { Song } from '../interfaces/Song';
 import { Constants } from '../app.constants';
+
+declare const window;
+
 
 @Component({
   selector: 'player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
+
+
 export class PlayerComponent implements OnInit {
-	query;
-  	results;
-  	constructor(private http: HttpClient) {}
+	results = [];
+	player1;
+  player2;
+  playA = false;
+  playB = false;
+  // soundCloudPlayer1 = document.getElementById("player-1");
+  // soundCloudPlayer2 = document.getElementById("player-2");
+
+  constructor(private http: HttpClient) {}
+
 
   	ngOnInit() {
+      // console.log(this.soundCloudPlayer2);
   	}
+
+  	playPause(deck){
+      if (deck == 1 ){
+        if (this.playA == false){
+          this.player1.play();
+          this.playA = true;
+        } else if (this.playA == true){
+          this.player1.pause();
+          this.playA = false;
+        }
+      } else if (deck == 2 ){
+        if (this.playB == false){
+          this.player2.play();
+          this.playB = true;
+        } else if (this.playB == true){
+          this.player2.pause();
+          this.playB = false;
+        }
+      }
+    }
+
+  	loadToDeck(deck, song){
+      const id = song.idFromProvider;
+      const uri = `https://api.soundcloud.com/tracks/${id}/stream?client_id=${Constants.API_KEY}`
+      if (deck == 1){
+        this.player1 = new Audio(uri);
+      } else if (deck == 2){
+        this.player2 = new Audio(uri);
+      }
+    }
 
   	searchSoundCloud(query: string) {
     	const maxResults = 100;
@@ -42,28 +84,35 @@ export class PlayerComponent implements OnInit {
         	}
     	})
     	.subscribe((result: any) => {
-        console.log(result);
+        this.results = result;
+        // console.log(this.results);
     	})
     }
 
    	handleResponse(res: any): any{
-		var data = res;
-		var result = [];
+		let data = res;
+		let result = [];
+    console.log(data);
+
 		if (data && data.collection) {
 			data.collection.forEach(function(item) {
 				var song: Song = <Song>{};
 				song.streamUrl = item.stream_url;
-				song.name = item.title;
-				song.artist = item.user.username;
+				song.title = item.title;
+				song.artist = item.artist; /// Undefined -> the response does not have an artist property.
 				song.provider = 1;
 				song.idFromProvider = item.id;
 				song.duration = item.duration;
 				song.imageUrl = item.artwork_url;
 				song.link = item.permalink_url;
+				song.waveform_url = item.waveform_url;
+				song.format = item.original_format;
 				result.push(song);
+
 			});
 		}
-
+      console.log(result);
 		return result;
+
 	}
 }
